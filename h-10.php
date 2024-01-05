46a323ed856d396a79826ca13625dcaeeb058b60
zMarvinnn <126242698+zMarvinnn@users.noreply.github.com>
2024-01-05 20:11:49 +0800

    jan-5-24

--- h-10.php
+++ h-10.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION["user"])) {
       <!---box1---->
     <div class="showcase-box-main">
     <a href="#" class="s1">
-      <img src="images/horror/a-l/l10.jpg"/>
+      <img src="images/horror/a-l/l10.jpeg"/>
     </a>
     </div>
   </section>
